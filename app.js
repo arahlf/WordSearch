cb03caa8dfe86ca1401971771332e0eb457c638d
arahlf <arahlf@gmail.com>
2014-03-04 21:38:59 -0600

    Using svg now instead of divs.

--- app.js
+++ app.js
@@ -5,16 +5,38 @@ $(function() {
             throw error;
         }
 
-        d3.select('body')
-            .selectAll('div')
-                .data(json)
+        var size = 30;
+        var spacing = 2;
+
+        var svg = d3.select('body')
+            .append('svg');
+
+        var group = svg.selectAll('g')
+            .data(json)
+            .enter()
+            .append('g')
+            .selectAll('g')
+                .data(function(data) { return data; })
                 .enter()
-                .append('div')
-                    .selectAll('span')
-                    .data(function(data) { return data; })
-                    .enter()
-                    .append('span')
-                    .text(function(data) { return data ; });
+                .append('g')
+                .attr('transform', function(data, x, y) {
+                    var x = (x * size) + (x * spacing);
+                    var y = (y * size) + (y * spacing);
+
+                    return 'translate(' + x + ', ' + y + ')';
+                });
+
+        group.append('rect')
+            .attr('width', size)
+            .attr('height', size);
+
+        group.append('text')
+            .text(function(data) {
+                return data;
+            })
+            .attr('y', size / 2 + 4)
+            .attr('x', size / 2 - 4);
+       
     });
 
 });
