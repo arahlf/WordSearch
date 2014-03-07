e200524e47d3e2e29e6a80402f3aabc4670042ca
arahlf <arahlf@gmail.com>
2014-03-06 21:07:45 -0600

    Applied some lovin'.

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ $(function() {
             throw error;
         }
 
+        var size = 30;
         var gridSize = json.length;
         var wordHighlightPadding = 2;
         var gridSize = json.length;
         var flattenedData = json.reduce(function(a, b) {
             return a.concat(b);
         });
-
-
-        var size = 30;
 
         var svg = d3.select('body')
             .append('svg');
@@ -36,39 +34,38 @@ $(function() {
             .attr('height', size - 2);
 
         group.append('text')
+            .attr('x', size / 2 - 5)
+            .attr('y', size / 2 + 5)
             .text(function(data) {
                 return data;
-            })
-            .attr('y', size / 2 + 5)
-            .attr('x', size / 2 - 5);
+            });
 
-
-
-        function strokeHorizontalWord(row, col, length) {
-            svg.append('rect')
-                .attr('x', (col * size) + wordHighlightPadding)
-                .attr('y', (row * size) + wordHighlightPadding)
-                .attr('width', (length * size) - (wordHighlightPadding * 2))
-                .attr('height', size - (wordHighlightPadding * 2))
+        function startWordHighlight() {
+            return svg.append('rect')
                 .attr('rx', 10)
                 .attr('ry', 10)
                 .attr('style', 'stroke: red; stroke-width: 2;');
         }
 
+        function strokeHorizontalWord(row, col, length) {
+            startWordHighlight()
+                .attr('x', (col * size) + wordHighlightPadding)
+                .attr('y', (row * size) + wordHighlightPadding)
+                .attr('width', (length * size) - (wordHighlightPadding * 2))
+                .attr('height', size - (wordHighlightPadding * 2));
+        }
+
         function strokeVerticalWord(row, col, length) {
-            svg.append('rect')
+            startWordHighlight()
                 .attr('x', (col * size) + wordHighlightPadding)
                 .attr('y', (row * size) + wordHighlightPadding)
                 .attr('width', size - (wordHighlightPadding * 2))
-                .attr('height', (length * size) - (wordHighlightPadding * 2))
-                .attr('rx', 10)
-                .attr('ry', 10)
-                .attr('style', 'stroke: red; stroke-width: 2;');
+                .attr('height', (length * size) - (wordHighlightPadding * 2));
         }
 
         function strokeDiagonalWord(row, col, length, forward) {
             var x, y, transform;
-            var side = 30 * (length - .5);
+            var side = size * (length - .5);
 
             if (forward) {
                 x = (col * size) + (size / 2) + wordHighlightPadding;
@@ -81,174 +78,165 @@ $(function() {
                 transform = 'rotate(-225, ' + x + ', ' + y + ')';
             }
 
-            svg.append('rect')
+            startWordHighlight()
                 .attr('x', x)
                 .attr('y', y)
                 .attr('width', size - (wordHighlightPadding * 2))
                 .attr('height', Math.sqrt(Math.pow(side, 2) + Math.pow(side, 2)))
-                .attr('rx', 10)
-                .attr('ry', 10)
-                .attr('transform', transform)
-                .attr('style', 'stroke: red; stroke-width: 2;');
+                .attr('transform', transform);
+        }
+
+        function checkNorth(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testRow = row - i;
+
+                if (testRow >= 0 && json[testRow][col] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeVerticalWord(row - word.length + 1, col, word.length);
+                        
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        function checkEast(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testCol = col + i;
+
+                if (testCol < gridSize && json[row][testCol] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeHorizontalWord(row, col, word.length);
+
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        function checkSouth(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testRow = row + i;
+
+                if (testRow < gridSize && json[testRow][col] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeVerticalWord(row, col, word.length);
+
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        function checkWest(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testCol = col - i;
+
+                if (testCol >= 0 && json[row][testCol] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeHorizontalWord(row, col - word.length + 1, word.length);
+
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        function checkNorthEast(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testRow = row - i;
+                var testCol = col + i;
+
+                if (testRow >= 0 && testCol < gridSize && json[testRow][testCol] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeDiagonalWord(row, col, word.length, true);
+                        
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        function checkSouthEast(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testRow = row + i;
+                var testCol = col + i;
+
+                if (testRow < gridSize && testCol < gridSize && json[testRow][testCol] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeDiagonalWord(testRow, testCol, word.length, false);
+
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        function checkSouthWest(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testRow = row + i;
+                var testCol = col - i;
+
+                if (testRow < gridSize && testCol >= 0 && json[testRow][testCol] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeDiagonalWord(testRow, testCol, word.length, true);
+                        
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        function checkNorthWest(word, letters, row, col) {
+            for (var i = 1; i < letters.length; i++) {
+                var testRow = row - i;
+                var testCol = col - i;
+
+                if (testRow >= 0 && testCol >= 0 && json[testRow][testCol] == letters[i]) {
+                    if (i == letters.length - 1) {
+                        strokeDiagonalWord(row, col, word.length, false);
+                        
+                        return true;
+                    }
+                }
+                else {
+                    return false;
+                }
+            }
         }
 
         function highlightWord(word) {
             var letters = word.split('');
-            var i, testRow, testCol;
-            var found;
+            var checkerFunctions = [checkNorth, checkNorthEast, checkEast, checkSouthEast, checkSouth, checkSouthWest, checkWest, checkNorthWest];
 
             for (var row = 0; row < gridSize; row++) {
                 for (var col = 0; col < gridSize; col++) {
                     if (letters[0] === json[row][col]) {
-                        found = false;
 
-                        // north
-                        for (i = 1; i < letters.length; i++) {
-                            testRow = row - i;
-
-                            if (testRow >= 0 && json[testRow][col] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeVerticalWord(row - word.length + 1, col, word.length);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        if (found) {
-                            return;
-                        }
-
-                        // east
-                        for (i = 1; i < letters.length; i++) {
-                            testCol = col + i;
-
-                            if (testCol < gridSize && json[row][testCol] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeHorizontalWord(row, col, word.length);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        if (found) {
-                            return;
-                        }
-
-                        // south
-                        for (i = 1; i < letters.length; i++) {
-                            testRow = row + i;
-
-                            if (testRow < gridSize && json[testRow][col] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeVerticalWord(row, col, word.length);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        if (found) {
-                            return;
-                        }
-
-                        // west
-                        for (i = 1; i < letters.length; i++) {
-                            testCol = col - i;
-
-                            if (testCol >= 0 && json[row][testCol] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeHorizontalWord(row, col - word.length + 1, word.length);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        if (found) {
-                            return;
-                        }
-
-                        // north east
-                        for (i = 1; i < letters.length; i++) {
-                            testRow = row - i;
-                            testCol = col + i;
-
-                            if (testRow >= 0 && testCol < gridSize && json[testRow][testCol] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeDiagonalWord(row, col, word.length, true);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        // south west
-                        for (i = 1; i < letters.length; i++) {
-                            testRow = row + i;
-                            testCol = col - i;
-
-                            if (testRow < gridSize && testCol >= 0 && json[testRow][testCol] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeDiagonalWord(testRow, testCol, word.length, true);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        if (found) {
-                            return;
-                        }
-
-                        // north west
-                        for (i = 1; i < letters.length; i++) {
-                            testRow = row - i;
-                            testCol = col - i;
-
-                            if (testRow >= 0 && testCol >= 0 && json[testRow][testCol] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeDiagonalWord(row, col, word.length, false);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
-
-                        if (found) {
-                            return;
-                        }
-
-                        // south east
-                        for (i = 1; i < letters.length; i++) {
-                            testRow = row + i;
-                            testCol = col + i;
-
-                            if (testRow < gridSize && testCol < gridSize && json[testRow][testCol] == letters[i]) {
-                                if (i == letters.length - 1) {
-                                    found = true;
-                                    strokeDiagonalWord(testRow, testCol, word.length, false);
-                                }
-                            }
-                            else {
-                                break;
-                            }
-                        }
+                        var found = (checkerFunctions.some(function(checkFunction) {
+                            return checkFunction(word, letters, row, col)
+                        }));
 
                         if (found) {
                             return;
@@ -257,7 +245,7 @@ $(function() {
                 }
             }
 
-            throw new Error('Did not find: ' + word);
+            throw new Error('Could not find: ' + word);
         }
 
 
